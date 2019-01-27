5c5e945bd658e35ccd5f92dc398395fe1b5f0b9f
raguljerish <46901224+raguljerish@users.noreply.github.com>
2019-01-27 11:09:06 +0530

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,13 @@
+#ragul jerish 
+year=int(input("enter the year :"))
+if year%4==0:
+    if year%100==0:
+        if year%400==0:
+            print("yes")
+        else:
+             print ("yes")
+    else :
+        print("yes")
+else:
+    print("yes")
+    
